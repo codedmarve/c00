c78b4e145ac206d3c66d559641cfc390832e822a
marve <codedmarve@gmail.com>
2022-06-12 23:43:42 +0100

    first commit

---
+++ ex06/ft_print_comb2.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moduwole <moduwole@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/10 23:01:36 by coder             #+#    #+#             */
+/*   Updated: 2022/02/18 21:19:30 by moduwole         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i <= 98)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			ft_putchar((i / 10) + '0');
+			ft_putchar((i % 10) + '0');
+			ft_putchar(' ');
+			ft_putchar((j / 10) + '0');
+			ft_putchar((j % 10) + '0');
+			if (i != 98)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			j =  + 1;	= 2
+		}
+		i = i + 1;
+	}
+}
